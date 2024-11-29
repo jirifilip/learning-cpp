6432172c98d15afd7c9f504e0958f01a32acfdba
Jiří Filip <j.f.ilip@seznam.cz>
2024-11-29 17:14:32 +0100

    Add start of exercise

--- run_tests.ps1
+++ run_tests.ps1
@@ -8,11 +8,13 @@ g++ `
 	-Wall -Wextra -Werror `
 	-I include `
 	-I $googletest_dir\googletest\include `
+	-I $googletest_dir\googlemock\include `
 	-L $googletest_dir\build\lib `
 	-L out `
 	tests/test.cpp `
 	-o out/test.exe `
 	-l gtest `
+	-l gmock `
 	-l gtest_main `
 	-l:lib.a
 
--- tests/test.cpp
+++ tests/test.cpp
@@ -1,8 +1,41 @@
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
 #include <lib.h>
+
+#include <memory>
+
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <utility>
+#include <iterator>
+
+
+std::vector<int> readNumbers(std::istream& input) {
+  std::string line;
+
+  std::getline(input, line);
+  std::istringstream line_stream(line);
+  std::istream_iterator<int> begin(line_stream), end;
+  
+  auto vector = std::vector<int>(begin, end); 
+  return vector;
+}
+
 
 
 TEST(LibTest, Plus) {
   ASSERT_EQ(plus(1, 2), 3);
   ASSERT_EQ(plus(20, 1), 21);
+}
+
+
+TEST(Exercise1, ReadNumbersTest) {
+  std::stringstream stream;
+  stream << "2 1 5 3 4";
+
+  auto v = readNumbers(stream);
+
+  ASSERT_THAT(v, ::testing::ElementsAre(2, 1, 5, 3, 4));
 }
